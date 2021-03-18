9f47fb696723a1a4a8ff411e4e3a0f7d6d0e2afc
kaworu-mg <70263755+kaworu-mg@users.noreply.github.com>
2021-03-18 14:52:55 +0900

    Update vgg16_kimettsu.py

--- vgg16_kimettsu.py
+++ vgg16_kimettsu.py
@@ -1,13 +1,18 @@
 #ライブラリのインポート
 import os
 import keras
-from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers import Input, Activation, Dropout, Flatten, Dense
 from keras import optimizers
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import time
+import glob
+%matplotlib inline
 
 #分類するクラス
 classes = ["inosuke","kanawo","nezuko","tanziro","zenitsu"]
@@ -85,3 +90,56 @@ plt.legend(loc="best")
 #Final.pngという名前で結果を保存
 plt.savefig("Final.png")
 plt.show()
+
+#モデルの保存
+import joblib
+joblib.dump(kimetsu_model,"kimetsu72")
+
+model = joblib.load("kimetsu72")
+
+data_list = ["inosuke_1.jpg","inosuke_2.jpg","inosuke_3.jpg","kanawo_1.jpg","kanawo_2.jpg","kanawo_3.jpg",
+            "nezuko_1.jpg","nezuko_2.jpg","nezuko_3.jpg","tanziro_1.jpg","tanziro_2.jpg","tanziro_3.jpg",
+            "zenitsu_1.jpg","zenitsu_2.jpg","zenitsu_3.jpg"]
+label_list = ["inosuke","kanawo","nezuko","tanziro","zenitsu"]
+
+
+for i in range(15):
+    img = mpimg.imread(data_list[i])
+    plt.subplot(3, 5, i+1)
+    plt.imshow(img)
+    plt.show()
+    
+    data_img = image.load_img(data_list[i], target_size=(150, 150))#画像を指定サイズで読み込み
+    data_arr = image.img_to_array(data_img)
+    data_arr = preprocess_input(data_arr)#画像の正規化。前処理
+    data_arr = data_arr.reshape(-1, 150, 150, 3)#画像を変換
+    pred = np.argmax(model.predict(data_arr) )#モデルVGG16を使ってプロダクションする
+    print(label_list[pred])
+
+kimetsu_model = joblib.load("kimetsu72")
+
+#テスト用のコード
+#画像を読み込んで予測する
+def img_predict(filename):
+    #画像を読み込んで4次元テンソルへ変換
+    img = image.load_img(filename, target_size=(img_height, img_width))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    #学習時にImageDataGeneratorのrescaleで正規化したので同じ処理が必要
+    x= x/255
+    #表示
+    plt.imshow(img)
+    plt.show()
+    #指数表記を禁止にする
+    np.set_printoptions(suppress=True)
+    
+    #画像のキャラを予測
+    pred = kimetsu_model.predict(x)[0]
+    #結果を表示する
+    print('"inosuke":0,"kanawo":1,"nezuko":2,"tanziro":3,"zenitsu":4')
+    print(pred*100)
+    
+#テスト用の画像データ読み込み
+test = glob.glob("./kimetsu_image/test/*/*")
+
+img_predict(test[15])
